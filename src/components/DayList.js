6355c08a2acb6d2d886cc7386c8f527597976321
Victoria Lee <victorialee@Victorias-MacBook-Pro-2.local>
2020-08-31 18:09:01 -0400

    Deleted dead code

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,46 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import DayListItem from "components/DayListItem"
-// import days from "./stories/index"
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 
 export default function DayList(props) {
-  const days = props.days.map(day => {
-    return (
-      <ul>
-      <DayListItem
+  return (
+    <ul>
+      {props.days.map(day => {
+        return <DayListItem
         key={day.id}
         name={day.name} 
         spots={day.spots} 
         selected={day.name === props.day}
         setDay={props.setDay}  
         />
-      </ul>
-    );
-  })
-  return days;
+      })
+      }
+    </ul>
+  )
 }
-
-ReactDOM.render(
-  <DayListItem days={days} />,
-  document.getElementById("root")
-);
