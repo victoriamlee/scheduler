ff435acb223cbda1432fb273c6f6d51074bffe98
Victoria Lee <victorialee@Victorias-MacBook-Pro-2.local>
2020-08-31 15:01:34 -0400

    Built button and daylistitem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,22 @@
+// import React, { useState } from 'react';
+import React from "react";
+
+import "components/DayListItem.scss";
+
+import classNames from 'classnames/bind';
+
+export default function DayListItem(props) {
+  // const [name, setDay] = useState(0);
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots}</h3>
+    </li>
+  );
+}
