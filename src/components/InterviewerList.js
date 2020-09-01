556651f515fa24969353c50d3ec2f222408adccf
Victoria Lee <victorialee@Victorias-MacBook-Pro-2.local>
2020-09-01 13:05:13 -0400

    Refactoring interviewer components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,19 +7,20 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
 
-
   return (
     <section className="interviewers">
   <h4 className="interviewers__header text--light">Interviewer</h4>
   <ul className="interviewers__list">
   {props.interviewers.map(interviewer => {
-        return <InterviewerListItem
+        return (
+        <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name} 
         avatar={interviewer.avatar} 
         selected={interviewer.id === props.interviewer}
-        setInterviewer={props.setInterviewer}  
+        setInterviewer={event => props.setInterviewer(interviewer.id)}  
         />
+        );
       })
       }
   </ul>
