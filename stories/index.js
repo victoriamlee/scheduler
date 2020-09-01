567d3289ceee5b68bab6e4702787ec2064ca608c
Victoria Lee <victorialee@Victorias-MacBook-Pro-2.local>
2020-09-01 13:31:59 -0400

    Created appointment component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem"
 import DayList from "components/DayList"
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
+import Appointment from "components/Appointment";
 
 storiesOf("Button", module)
   .addParameters({
@@ -129,3 +130,10 @@ storiesOf("Button", module)
             setInterviewer={action("setInterviewer")}
           />
         ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with time", () => <Appointment time="12pm" />);
