8e9bd429fc449495bbff91e74e2d0422fbd08c0c
Victoria Lee <victorialee72@live.com>
2020-09-07 10:13:55 -0400

    Set up prop-types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import InterviewerListItem from "components/InterviewerListItem"
 
@@ -27,3 +28,7 @@ export default function InterviewerList(props) {
 </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
