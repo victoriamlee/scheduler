61fa9b0c7de26def98e3063df559baef30588500
Victoria Lee <victorialee72@live.com>
2020-09-08 13:58:33 -0400

    Added debugging tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -16,7 +16,7 @@ it("defaults to Monday and changes the schedule when a new day is selected", () 
   });
 });
 it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-  const { container } = render(<Application />);
+  const { container, debug } = render(<Application />);
 
   await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -32,6 +32,15 @@ fireEvent.click(getByAltText(appointment, "Tori Malcolm"));
 
 fireEvent.click(getByText(appointment, "Save"));
 
-  console.log(prettyDOM(appointment));
+expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+const day = getAllByTestId(container, "day").find(day => 
+  queryByText(day, "Monday")
+  );
+
+  expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
 })
 })
